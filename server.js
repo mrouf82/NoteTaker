8382d4b428c69c526082ae114ad6da73135fd8cd
Mohammad Rouf <mrouf82@gmail.com>
2021-03-17 17:56:11 -0400

    changed directory paths

---
+++ server.js
@@ -0,0 +1,11 @@
+//require the dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
